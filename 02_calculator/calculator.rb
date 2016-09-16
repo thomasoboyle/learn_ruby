8e9c648f19a00c41f44c161bef3c8772547dd399
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-09-15 21:46:48 -0700

    First too solutions to calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,12 @@
-#write your code here
+def add(number1, number2)
+  new_number = number1 + number2
+end
+
+def subtract(number1, number2)
+  new_number = number1 - number2
+end
+
+def sum # I need to pass paramaters to as an array
+          # here I need to loop through the paramaters
+          # and add them together. 
+end
