198a3dfb70a1264d91c46f9bcc008277159de342
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-09-17 09:25:44 -0700

    1st two solutions to simon

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,11 @@
-#write your code here
+def echo(words)
+  words
+end
+
+def shout(words)
+  words.upcase
+end
+
+def repeat(words, times=2)
+  (words + " ") * times # leaves a trailing space :/
+end
