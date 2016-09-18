6812dff64269a2d70e89e9758b799bb7893a2772
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-09-18 15:25:12 -0700

    Uses indexing and start_with? to react to lettering

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,4 +1,10 @@
 def translate(word)
-  word_end = "ay"
+  if word.start_with?("a", "e", "i", "o", "u")
+    word_end = "ay"
+  else
+    letter = word[0]
+    word = word.delete!(letter)
+    word_end = letter + "ay"
+  end
   word + word_end
 end
