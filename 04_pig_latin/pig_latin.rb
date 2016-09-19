9ace4dd1f3855439991c8aa0201b6ba519a59157
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-09-18 17:17:39 -0700

    Solution to words starting with two consonants

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,7 +4,15 @@ def translate(word)
   else
     letter = word[0]
     word = word.delete!(letter)
-    word_end = letter + "ay"
+
+    if word.start_with?("a", "e", "i", "o", "u")
+      word_end = letter + "ay"
+    else
+      second_letter = word[0]
+      word = word.delete!(second_letter)
+      word_end = letter + second_letter + "ay"
+    end
+
   end
   word + word_end
 end
