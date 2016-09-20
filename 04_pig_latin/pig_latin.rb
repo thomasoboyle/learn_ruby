b28b0685b0df748bbfa03f05528e75ff134ca4b2
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-09-19 21:23:51 -0700

    Tried using module to separate concerns and messed everything up

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,18 +1,26 @@
-def translate(word)
-  if word.start_with?("a", "e", "i", "o", "u")
-    word_end = "ay"
-  else
-    letter = word[0]
-    word = word.delete!(letter)
+module Pigtalker
 
-    if word.start_with?("a", "e", "i", "o", "u")
-      word_end = letter + "ay"
-    else
-      second_letter = word[0]
-      word = word.delete!(second_letter)
-      word_end = letter + second_letter + "ay"
-    end
-
+  def translate(phrase)
+    array = phrase.split(' ')
   end
-  word + word_end
+
+  def piggie(array)
+    array.each do |i|
+      if start_with?("a", "e", "i", "o", "u")
+        word_end = "ay"
+      else
+        letter = array[0]
+        array = array.delete!(letter)
+        if array.start_with?("a", "e", "i", "o", "u")
+          word_end = letter + "ay"
+        else
+          second_letter = array[0]
+          array = array.delete!(second_letter)
+          word_end = letter + second_letter + "ay"
+        end
+      end
+      array + word_end
+    end
+  end
+
 end
