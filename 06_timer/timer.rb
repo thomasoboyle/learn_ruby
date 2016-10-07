c34a97fa6966c03bfac443a4dc2a56a238b17602
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-10-06 19:44:21 -0700

    Mostlye notes

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -9,7 +9,22 @@ class Timer
     @sec
   end
 
-  def time_string
-    "00:00:0#{seconds}"
+  def divider
+    if @sec < 10
+      "0#{@sec}"
+    elsif @sec >= 10
+      tens = 1 + "#{@sec}"%10
+
+    end
+
   end
+
+
+  def time_string
+    "00:00:#{divider}" #maybe there is a chance for
+  end                  # a method called divider
 end
+
+#above takes care of the ones place
+# Need to figure out a way to take car of the other places
+# i.e. Tens, minutes - ones, minutes -
