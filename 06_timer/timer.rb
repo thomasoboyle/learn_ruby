314d1f7ca738b64f724a15ec98214713e4af7746
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-10-08 12:44:37 -0700

    Solves for seconds. Wanted to commit before handling complicated time_string logic

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,28 +1,36 @@
 class Timer
-  attr_writer :seconds
+  attr_accessor :seconds
 
   def initialize
-    @sec = 0
-  end
-
-  def seconds
-    @sec
+    @seconds = 0
   end
 
   def divider
-    if @sec < 10
-      "0#{@sec}"
-    elsif @sec >= 10
-      tens = 1 + "#{@sec}"%10
-
+    if @seconds < 10
+      puts @seconds
+      @seconds = sprintf("%02d", @seconds)
+    elsif @seconds >= 10 && @seconds < 60
+      ones = @seconds%10
+      tens = @seconds/10
+      "#{tens}" + "#{ones}"
+    elsif minutes
+      #minutes method
     end
-
   end
 
 
   def time_string
-    "00:00:#{divider}" #maybe there is a chance for
-  end                  # a method called divider
+    "00:00:#{divider}"
+  end
+
+  def minutes
+    if @seconds >= 60
+      hundreds  = @seconds%60
+      thousands = @seconds/60
+    else
+    'kats'
+    end
+  end 
 end
 
 #above takes care of the ones place
