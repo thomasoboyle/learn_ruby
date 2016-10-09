75da28df14bc158b5015d46909278bfc39189774
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-10-08 22:14:33 -0700

    Making messes

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,32 +1,45 @@
 class Timer
-  attr_accessor :seconds
+  attr_accessor :seconds,
+                :minutes,
+                :hours
 
   def initialize
     @seconds = 0
+    @minutes = sprintf("%02d", 0)
+    @hours   = sprintf("%02d", 0)
   end
 
-  def divider
+  def spliter
     if @seconds < 10
       puts @seconds
       @seconds = sprintf("%02d", @seconds)
+      @minutes = "00"
+      @hours   = "00"
     elsif @seconds >= 10 && @seconds < 60
       ones = @seconds%10
       tens = @seconds/10
-      "#{tens}" + "#{ones}"
-    elsif minutes
-      #minutes method
+      @seconds = "#{tens}" + "#{ones}"
+      @minutes = "00"
+      @hours   = "00"
+    elsif minutes_calc
     end
   end
 
 
   def time_string
-    "00:00:#{divider}"
+    if @seconds = 0
+      "00:00:00"
+    else
+    "#{@hours}:#{@minutes}:#{@seconds}"
+    end
   end
 
-  def minutes
-    if @seconds >= 60
-      hundreds  = @seconds%60
-      thousands = @seconds/60
+  def minutes_calc
+    if @seconds >= 60 && @seconds > 3600
+      @minutes   = @seconds%60
+      left_over  = hundreds%10
+      @seconds   = "#{@minutes}" + "#{left_over}" 
+      thousands  = @seconds/60
     else
     'kats'
     end
