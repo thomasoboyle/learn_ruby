ad8cacb0f83cb61a15981a64bd905b519a882bc2
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-10-08 23:11:56 -0700

    functional solution

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,48 +1,23 @@
 class Timer
-  attr_accessor :seconds,
-                :minutes,
-                :hours
+  attr_accessor :seconds
 
   def initialize
     @seconds = 0
-    @minutes = sprintf("%02d", 0)
-    @hours   = sprintf("%02d", 0)
   end
 
-  def spliter
-    if @seconds < 10
-      @seconds = sprintf("%02d", @seconds)
-      @minutes = "00"
-      @hours   = "00"
-    elsif seconds_calc
-      puts "heellldfjdalfjaf*********"
-    else minutes_calc
-    end
+  def seconds_calc(total_seconds)
+    sprintf("%02d", total_seconds%60)
+  end
+
+  def minutes_calc(total_seconds)
+    sprintf("%02d", (total_seconds/60)%60)
+  end
+
+  def hours_calc(total_seconds)
+    sprintf("%02d", total_seconds/3600)
   end
 
   def time_string
-    spliter
-    "#{@hours}:#{@minutes}:#{@seconds}"
+    "#{hours_calc(@seconds)}:#{minutes_calc(@seconds)}:#{seconds_calc(@seconds)}"
   end
-
-
-
-  def seconds_calc
-    puts "++++++++++++++seconds_cal___________+"
-    @seconds >= 10 && @seconds < 60
-    ones = @seconds%10
-    tens = @seconds/10
-    @seconds = "#{tens}" + "#{ones}" #sprintf to 12 here
-    @minutes = "00"
-    @hours   = "00"
-  end
-
-  def minutes_calc
-    puts "********fjkalfjalsdf**********"
-    @seconds >= 60 && @seconds > 3600
-    @minutes   = @seconds/60
-    ones       = @seconds%10
-    tens       = @seconds/10
-  end 
 end
-
