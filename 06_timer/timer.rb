c6d3d4407b2e422519e825d4bf6c18fea38b776f
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-10-08 22:47:02 -0700

    minutes_calc isn't being called because seconds_calc is truthy

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -15,34 +15,34 @@ class Timer
       @minutes = "00"
       @hours   = "00"
     elsif seconds_calc
-    elsif minutes_calc
+      puts "heellldfjdalfjaf*********"
+    else minutes_calc
     end
   end
-
 
   def time_string
     spliter
     "#{@hours}:#{@minutes}:#{@seconds}"
   end
 
+
+
   def seconds_calc
+    puts "++++++++++++++seconds_cal___________+"
     @seconds >= 10 && @seconds < 60
     ones = @seconds%10
     tens = @seconds/10
-    @seconds = "#{tens}" + "#{ones}"
+    @seconds = "#{tens}" + "#{ones}" #sprintf to 12 here
     @minutes = "00"
     @hours   = "00"
   end
 
   def minutes_calc
+    puts "********fjkalfjalsdf**********"
     @seconds >= 60 && @seconds > 3600
-    @minutes   = @seconds%60
-    left_over  = hundreds%10
-    @seconds   = "#{@minutes}" + "#{left_over}" 
-    thousands  = @seconds/60
+    @minutes   = @seconds/60
+    ones       = @seconds%10
+    tens       = @seconds/10
   end 
 end
 
-#above takes care of the ones place
-# Need to figure out a way to take car of the other places
-# i.e. Tens, minutes - ones, minutes -
